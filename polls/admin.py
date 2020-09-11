57c98725e6761ea95c8757c03d349a4e4a06ee48
Siwei <siwei_0408@hotmail.com>
2020-09-11 16:41:13 +0800

    create polls

---
+++ polls/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from .models import Questions, Choice
+
+
+# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Lul,here is something',               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'],'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    search_fields = ['question_text']
+    list_display = ('question_text', 'pub_date','was_published_recently')
+    list_filter = ['pub_date']
+
+
+admin.site.register(Questions, QuestionAdmin)
+
+
